a4913dc8c62db8fc2aaf60be8eb032272de716eb
lukas.petrikas <lukas.petrikas@teltonika.lt>
2023-08-21 15:23:34 +0300

    replacing table method

---
+++ main/scripts.js
@@ -0,0 +1,84 @@
+
+
+
+
+hide_div("test_div");
+
+//ADJUST COLOR
+const colorInput = document.getElementById('color');
+const lights = document.querySelectorAll('.running_lights ul li');
+const lights_circle = document.querySelectorAll('.circle');
+const circleContainers = document.querySelectorAll('.circle-container');
+
+      circleContainers.forEach(container => {
+      changeBoxShadowColorInAnimation(container, '#ffff00');
+  });
+  
+
+  colorInput.addEventListener('input', () => {
+    const newColor = colorInput.value;
+    for (let i = 0; i < lights.length; i++) {
+      lights_circle[i].style.backgroundColor = `${newColor}`;
+    }
+    circleContainers.forEach(container => {
+      changeBoxShadowColorInAnimation(container, newColor);
+    });
+  });
+
+
+
+
+// Repeat for other circles
+
+// Function to change box shadow color within the animation
+function changeBoxShadowColorInAnimation(element, color) {
+  element.style.setProperty('--box-shadow-color', color);
+}
+//END OF ADJUST COLOR
+
+
+//CHANGE SPEED
+const speedSlider = document.getElementById('speed');
+console.log("speed id = ",speed);
+
+const circles = document.querySelectorAll('.circle');
+
+function updateAnimationSpeed() {
+  console.log("adjusting speed ");
+  const animationSpeed = 6 - speedSlider.value; // Inverse mapping from slider value to animation speed
+  circles.forEach(circle => {
+    circle.style.animation = `grow ${animationSpeed}s ease-in-out infinite`;
+  });
+}
+
+speedSlider.addEventListener('input', updateAnimationSpeed);
+//END OF CHANGE SPEED
+
+
+
+
+
+function user_mode_popup_configure(sel) {
+  let user_mode_popup = document.getElementById(sel);
+  user_mode_popup.innerHTML = ``;
+  user_mode_popup.style.display = "none";
+}
+
+function user_mode_popup_cancel(sel) {
+  let user_mode_popup = document.getElementById(sel);
+  user_mode_popup.innerHTML = ``;
+  user_mode_popup.style.display = "none";
+}
+
+
+function hide_div(div) {
+  console.log("hiding div");
+  var document_div = document.getElementById(div);
+  document_div.setAttribute("hidden", "hidden");
+}
+
+function unhide_div(div) {
+  console.log("unhiding div");
+  var document_div = document.getElementById(div);
+  document_div.removeAttribute("hidden");
+}
